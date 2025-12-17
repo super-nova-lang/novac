744b0d0dea1e9c6d42255e162338919dd26ff5a7
Ashton Sickler <ashton.sickler06@gmail.com>
2025-12-16 20:41:32 -0600

    Better testing

--- lib/lexer_test.ml
+++ lib/lexer_test.ml
@@ -1,222 +1,195 @@
-open Token
-
-let reset_ppf = Spectrum.prepare_ppf Format.std_formatter
-
-let rec print_diff ppx xs ys =
-  match xs, ys with
-  | x :: xt, y :: yt when x = y -> print_diff ppx xt yt
-  | x :: xt, y :: yt ->
-    Format.printf
-      "@{<red>Expected@}: @{<bold>%s@}, @{<green>Got@}: @{<bold>%s@}\n"
-      (ppx x)
-      (ppx y);
-    print_diff ppx xt yt
-  | x :: xt, [] ->
-    Format.printf
-      "@{<red>Expected@}: @{<bold>%s@}, @{<green>Got@}: @{<bold>None@}\n"
-      (ppx x);
-    print_diff ppx xt []
-  | [], y :: yt ->
-    Format.printf
-      "@{<red>Expected@}: @{<bold>None@}, @{<green>Got@}: @{<bold>%s@}\n"
-      (ppx y);
-    print_diff ppx [] yt
-  | [], [] -> ()
+let rec print_diff ppx xs =
+  match xs with
+  | x :: xt ->
+    Format.printf "%s\n" (ppx x);
+    print_diff ppx xt
+  | [] -> ()
 ;;
 
-let test ~expected ~got =
-  if expected = got
-  then true
-  else (
-    print_diff Token.show expected got;
-    reset_ppf ();
-    false)
-;;
+let test toks = print_diff Token.show toks
 
-let spec_tokens =
-  (* open Testing *)
-  [ Open
-  ; Ident "Testing" (* open Panic *)
-  ; Open
-  ; Ident "Panic"
-  ; Let (* let add :: a: i32, b: i32 -> i32 = {a + b} *)
-  ; Ident "add"
-  ; Double_colon
-  ; Ident "a"
-  ; Colon
-  ; Ident "i32"
-  ; Comma
-  ; Ident "b"
-  ; Colon
-  ; Ident "i32"
-  ; Skinny_arrow
-  ; Ident "i32"
-  ; Eql
-  ; Open_brack
-  ; Ident "a"
-  ; Plus
-  ; Ident "b"
-  ; Close_brack
-  ; Let (* let sub :: a, b = return a - b;*)
-  ; Ident "sub"
-  ; Double_colon
-  ; Ident "a"
-  ; Comma
-  ; Ident "b"
-  ; Eql
-  ; Return
-  ; Ident "a"
-  ; Dash
-  ; Ident "b"
-  ; Semi_colon
-  ; Let (* let mul :: a, b = { a * b } *)
-  ; Ident "mul"
-  ; Double_colon
-  ; Ident "a"
-  ; Comma
-  ; Ident "b"
-  ; Eql
-  ; Open_brack
-  ; Ident "a"
-  ; Star
-  ; Ident "b"
-  ; Close_brack
-  ; Let (* let mul_5 :: mul <- 5; *)
-  ; Ident "mul_5"
-  ; Double_colon
-  ; Ident "mul"
-  ; Back_arrow
-  ; Number 5
-  ; Semi_colon
-  ; Hash (* #[recursive] *)
-  ; Open_square
-  ; Ident "recursive"
-  ; Close_square
-  ; Let (* let factorial :: of = { *)
-  ; Ident "factorial"
-  ; Double_colon
-  ; Ident "of"
-  ; Eql
-  ; Open_brack
-  ; Match (* match of with *)
-  ; Ident "of"
-  ; With
-  ; Bar (* | 0 -> 0*)
-  ; Number 0
-  ; Skinny_arrow
-  ; Number 1
-  ; Bar (* | c if x < 0 -> panic!("...", x) *)
-  ; Ident "x"
-  ; If
-  ; Ident "x"
-  ; Lesser
-  ; Number 0
-  ; Skinny_arrow
-  ; Ident "panic"
-  ; Bang
-  ; Open_paren
-  ; String "factroial is not defined for negative numbers: got: {}"
-  ; Comma
-  ; Ident "x"
-  ; Close_paren
-  ; Bar (* | x -> x * factorial(x - 1) *)
-  ; Ident "x"
-  ; Skinny_arrow
-  ; Ident "x"
-  ; Star
-  ; Ident "factorial"
-  ; Open_paren
-  ; Ident "x"
-  ; Dash
-  ; Number 1
-  ; Close_paren
-  ; Close_brack (* } *)
-  ; Let (* let main :: () = { *)
-  ; Ident "main"
-  ; Double_colon
-  ; Open_paren
-  ; Close_paren
-  ; Eql
-  ; Open_brack
-  ; Let
-  ; Ident "res"
-  ; Eql
-  ; Ident "add"
-  ; Open_paren
-  ; Number 1
-  ; Comma
-  ; Number 5
-  ; Close_paren
-  ; Semi_colon
-  ; Ident "assert"
-  ; Bang
-  ; Open_paren
-  ; Ident "res"
-  ; Double_eql
-  ; Number 6
-  ; Close_paren
-  ; Semi_colon
-  ; Let
-  ; Ident "res"
-  ; Eql
-  ; Ident "sub"
-  ; Open_paren
-  ; Number 5
-  ; Comma
-  ; Number 15
-  ; Close_paren
-  ; Semi_colon
-  ; Ident "assert"
-  ; Bang
-  ; Open_paren
-  ; Ident "res"
-  ; Double_eql
-  ; Dash
-  ; Number 10
-  ; Close_paren
-  ; Semi_colon
-  ; Let
-  ; Ident "res"
-  ; Eql
-  ; Ident "mul"
-  ; Open_paren
-  ; Number 2
-  ; Comma
-  ; Number 5
-  ; Close_paren
-  ; Semi_colon
-  ; Ident "assert"
-  ; Bang
-  ; Open_paren
-  ; Ident "res"
-  ; Double_eql
-  ; Number 10
-  ; Close_paren
-  ; Semi_colon
-  ; Let
-  ; Ident "res_curry"
-  ; Eql
-  ; Ident "mul_5"
-  ; Open_paren
-  ; Number 10
-  ; Close_paren
-  ; Semi_colon
-  ; Ident "assert"
-  ; Bang
-  ; Open_paren
-  ; Ident "res_curry"
-  ; Double_eql
-  ; Number 10
-  ; Star
-  ; Number 5
-  ; Close_paren
-  ; Semi_colon
-  ; Close_brack
-  ; Eof
-  ]
-;;
-
-let%test "test spec" =
+let%expect_test "test spec" =
   let tokens = Lexer.lex Spec.content in
-  test ~got:tokens ~expected:spec_tokens
+  test tokens;
+  [%expect
+    {|
+    Token.Open
+    (Token.Ident "Testing")
+    Token.Open
+    (Token.Ident "Panic")
+    Token.Let
+    (Token.Ident "add")
+    Token.Double_colon
+    (Token.Ident "a")
+    Token.Colon
+    (Token.Ident "i32")
+    Token.Comma
+    (Token.Ident "b")
+    Token.Colon
+    (Token.Ident "i32")
+    Token.Skinny_arrow
+    (Token.Ident "i32")
+    Token.Eql
+    Token.Open_brack
+    (Token.Ident "a")
+    Token.Plus
+    (Token.Ident "b")
+    Token.Close_brack
+    Token.Let
+    (Token.Ident "sub")
+    Token.Double_colon
+    (Token.Ident "a")
+    Token.Comma
+    (Token.Ident "b")
+    Token.Eql
+    Token.Return
+    (Token.Ident "a")
+    Token.Dash
+    (Token.Ident "b")
+    Token.Semi_colon
+    Token.Let
+    (Token.Ident "mul")
+    Token.Double_colon
+    (Token.Ident "a")
+    Token.Comma
+    (Token.Ident "b")
+    Token.Eql
+    Token.Open_brack
+    (Token.Ident "a")
+    Token.Star
+    (Token.Ident "b")
+    Token.Close_brack
+    Token.Let
+    (Token.Ident "mul_5")
+    Token.Double_colon
+    (Token.Ident "mul")
+    Token.Back_arrow
+    (Token.Number 5)
+    Token.Semi_colon
+    Token.Hash
+    Token.Open_square
+    (Token.Ident "recursive")
+    Token.Close_square
+    Token.Let
+    (Token.Ident "factorial")
+    Token.Double_colon
+    (Token.Ident "of")
+    Token.Eql
+    Token.Open_brack
+    Token.Match
+    (Token.Ident "of")
+    Token.With
+    Token.Bar
+    (Token.Number 0)
+    Token.Skinny_arrow
+    (Token.Number 1)
+    Token.Bar
+    (Token.Ident "x")
+    Token.If
+    (Token.Ident "x")
+    Token.Lesser
+    (Token.Number 0)
+    Token.Skinny_arrow
+    (Token.Ident "panic")
+    Token.Bang
+    Token.Open_paren
+    (Token.String "factroial is not defined for negative numbers: got: {}")
+    Token.Comma
+    (Token.Ident "x")
+    Token.Close_paren
+    Token.Bar
+    (Token.Ident "x")
+    Token.Skinny_arrow
+    (Token.Ident "x")
+    Token.Star
+    (Token.Ident "factorial")
+    Token.Open_paren
+    (Token.Ident "x")
+    Token.Dash
+    (Token.Number 1)
+    Token.Close_paren
+    Token.Close_brack
+    Token.Let
+    (Token.Ident "main")
+    Token.Double_colon
+    Token.Open_paren
+    Token.Close_paren
+    Token.Eql
+    Token.Open_brack
+    Token.Let
+    (Token.Ident "res")
+    Token.Eql
+    (Token.Ident "add")
+    Token.Open_paren
+    (Token.Number 1)
+    Token.Comma
+    (Token.Number 5)
+    Token.Close_paren
+    Token.Semi_colon
+    (Token.Ident "assert")
+    Token.Bang
+    Token.Open_paren
+    (Token.Ident "res")
+    Token.Double_eql
+    (Token.Number 6)
+    Token.Close_paren
+    Token.Semi_colon
+    Token.Let
+    (Token.Ident "res")
+    Token.Eql
+    (Token.Ident "sub")
+    Token.Open_paren
+    (Token.Number 5)
+    Token.Comma
+    (Token.Number 15)
+    Token.Close_paren
+    Token.Semi_colon
+    (Token.Ident "assert")
+    Token.Bang
+    Token.Open_paren
+    (Token.Ident "res")
+    Token.Double_eql
+    Token.Dash
+    (Token.Number 10)
+    Token.Close_paren
+    Token.Semi_colon
+    Token.Let
+    (Token.Ident "res")
+    Token.Eql
+    (Token.Ident "mul")
+    Token.Open_paren
+    (Token.Number 2)
+    Token.Comma
+    (Token.Number 5)
+    Token.Close_paren
+    Token.Semi_colon
+    (Token.Ident "assert")
+    Token.Bang
+    Token.Open_paren
+    (Token.Ident "res")
+    Token.Double_eql
+    (Token.Number 10)
+    Token.Close_paren
+    Token.Semi_colon
+    Token.Let
+    (Token.Ident "res_curry")
+    Token.Eql
+    (Token.Ident "mul_5")
+    Token.Open_paren
+    (Token.Number 10)
+    Token.Close_paren
+    Token.Semi_colon
+    (Token.Ident "assert")
+    Token.Bang
+    Token.Open_paren
+    (Token.Ident "res_curry")
+    Token.Double_eql
+    (Token.Number 10)
+    Token.Star
+    (Token.Number 5)
+    Token.Close_paren
+    Token.Semi_colon
+    Token.Close_brack
+    Token.Eof
+    |}]
 ;;
